679c9a269613382731dbdd563a7bce5fa4f462c4
Иван <jazzman80@yandex.ru>
2024-08-18 18:40:55 +0300

    CI

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'package:get_server/get_server.dart';
 
 void main() {
-  runApp(
-    GetServerApp(
-      port: 443,
-      home: Home(),
-    ),
-  );
+  runApp(GetServerApp(
+    home: Home(),
+  ));
 }
 
 class Home extends StatelessWidget {
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Text('Welcome to fucking GetX!');
